35d59ab10f673e44d30a33c1b0661f4be7722071
Civitaspo <civitaspo@gmail.com>
2018-07-26 14:15:24 +0900

    Add operatorFactory FactoryMethod to OperatorProvider to remove each OperatorFactory

--- src/main/scala/pro/civitaspo/digdag/plugin/emr_fleet/EmrFleetPlugin.scala
+++ src/main/scala/pro/civitaspo/digdag/plugin/emr_fleet/EmrFleetPlugin.scala
@@ -1,17 +1,37 @@
 package pro.civitaspo.digdag.plugin.emr_fleet
 
 import java.util
+import java.lang.reflect.Constructor
 
-import io.digdag.spi.{OperatorFactory, OperatorProvider, Plugin}
+import io.digdag.client.config.Config
+import io.digdag.spi.{Operator, OperatorContext, OperatorFactory, OperatorProvider, Plugin, TemplateEngine}
+import javax.inject.Inject
+import pro.civitaspo.digdag.plugin.emr_fleet.operator.{AbstractEmrFleetOperator, EmrFleetListClustersOperator}
 
 object EmrFleetPlugin {
 
   class EmrFleetOperatorProvider extends OperatorProvider {
 
+    @Inject protected var systemConfig: Config = null
+    @Inject protected var templateEngine: TemplateEngine = null
+
     override def get(): util.List[OperatorFactory] = {
-      util.Arrays.asList()
+      util.Arrays.asList(
+        operatorFactory("emr_fleet.list_clusters", classOf[EmrFleetListClustersOperator])
+      )
     }
 
+    private def operatorFactory[T <: AbstractEmrFleetOperator](operatorName: String, klass: Class[T]): OperatorFactory = {
+      new OperatorFactory {
+        override def getType: String = operatorName
+        override def newOperator(context: OperatorContext): Operator = {
+          val constructor: Constructor[T] = klass.getConstructor(
+            classOf[String], classOf[OperatorContext], classOf[Config], classOf[TemplateEngine]
+          )
+          constructor.newInstance(getType, context, systemConfig, templateEngine)
+        }
+      }
+    }
   }
 }
 
