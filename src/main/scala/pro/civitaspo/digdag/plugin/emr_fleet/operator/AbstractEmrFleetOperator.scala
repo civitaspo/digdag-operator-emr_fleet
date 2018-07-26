462ae60424f1da6bfb04dd97940155bf0a45da08
Civitaspo <civitaspo@gmail.com>
2018-07-26 14:15:24 +0900

    Add operatorFactory FactoryMethod to OperatorProvider to remove each OperatorFactory

---
+++ src/main/scala/pro/civitaspo/digdag/plugin/emr_fleet/operator/AbstractEmrFleetOperator.scala
@@ -0,0 +1,17 @@
+package pro.civitaspo.digdag.plugin.emr_fleet.operator
+
+import io.digdag.client.config.Config
+import io.digdag.spi.{OperatorContext, TemplateEngine}
+import io.digdag.util.BaseOperator
+import org.slf4j.{Logger, LoggerFactory}
+
+abstract class AbstractEmrFleetOperator(
+  operatorName: String,
+  context: OperatorContext,
+  systemConfig: Config,
+  templateEngine: TemplateEngine
+) extends BaseOperator(context) {
+
+  protected val logger: Logger = LoggerFactory.getLogger(this.getClass)
+
+}
