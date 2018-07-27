7439edd72e5efd99f4f8342b481673c5b4c0b6ae
Civitaspo <civitaspo@gmail.com>
2018-07-27 09:11:58 +0900

    Impl EmrFleetDetectClustersOperator

--- src/main/scala/pro/civitaspo/digdag/plugin/emr_fleet/operator/EmrFleetDetectClustersOperator.scala
+++ src/main/scala/pro/civitaspo/digdag/plugin/emr_fleet/operator/EmrFleetDetectClustersOperator.scala
@@ -1,8 +1,15 @@
 package pro.civitaspo.digdag.plugin.emr_fleet.operator
 
-import com.amazonaws.services.elasticmapreduce.model.{ClusterState, ListClustersRequest}
-import io.digdag.client.config.Config
+import java.time.{LocalDateTime, ZoneId, ZoneOffset}
+import java.util.Date
+
+import com.amazonaws.services.elasticmapreduce.model.{ClusterState, ClusterSummary, ListClustersRequest, ListClustersResult}
+import com.google.common.collect.ImmutableList
+import io.digdag.client.config.{Config, ConfigKey}
 import io.digdag.spi.{OperatorContext, TaskResult, TemplateEngine}
+
+import scala.collection.JavaConverters._
+
 
 class EmrFleetDetectClustersOperator(
   context: OperatorContext,
@@ -10,23 +17,69 @@ class EmrFleetDetectClustersOperator(
   templateEngine: TemplateEngine
 ) extends AbstractEmrFleetOperator(context, systemConfig, templateEngine) {
 
+  val timezone: String = params.get("timezone", classOf[String])
   val hoursCreatedWithin: Int = params.get("hours_created_within", classOf[Int])
   val regexp: String = params.get("regexp", classOf[String], ".*")
   val states: Seq[ClusterState] = {
     val list = params.getListOrEmpty("states", classOf[String])
     if (list.isEmpty) Seq(ClusterState.RUNNING, ClusterState.WAITING)
-    else {
-      val builder = Seq.newBuilder[ClusterState]
-      for (s: String <- list) {
-        builder += ClusterState.fromValue(s)
-      }
-      builder.result()
-    }
+    else list.asScala.map(ClusterState.fromValue)
   }
 
   override def runTask(): TaskResult = {
-    emr.listClusters(new ListClustersRequest())
-    TaskResult.empty(request)
+    val clusters = detectClusters()
+
+    val p = newEmptyParams
+    val lastDetection = p.getNestedOrSetEmpty("emr_fleet").getNestedOrSetEmpty("last_detection")
+    lastDetection.set("is_detected", clusters.nonEmpty)
+    lastDetection.set("clusters", seqAsJavaList(clusters.map(clusterSummaryToDetectedClusterSummary)))
+
+    val builder = TaskResult.defaultBuilder(request)
+    builder.storeParams(p)
+    builder.resetStoreParams(ImmutableList.of(ConfigKey.of("emr_fleet", "last_detection")))
+    builder.build()
+  }
+
+  private def clusterSummaryToDetectedClusterSummary(cs: ClusterSummary): Config = {
+    val p = newEmptyParams
+    p.set("id", cs.getId)
+    p.set("name", cs.getName)
+    p.set("normalized_instance_hours", cs.getNormalizedInstanceHours)
+    p.set("current_state", cs.getStatus.getState)
+    p.set("last_state_change_reason_code", cs.getStatus.getStateChangeReason.getCode)
+    p.set("last_state_change_reason_message", cs.getStatus.getStateChangeReason.getMessage)
+    p.set("created_at", cs.getStatus.getTimeline.getCreationDateTime.getTime)
+    p.set("end_at", cs.getStatus.getTimeline.getEndDateTime.getTime)
+    p.set("ready_at", cs.getStatus.getTimeline.getReadyDateTime.getTime)
+    p
+  }
+
+  private def detectClusters(maker: Option[String] = None): Seq[ClusterSummary] = {
+    val req = new ListClustersRequest()
+      .withClusterStates(states: _*)
+      .withCreatedAfter(createdAfter)
+    maker match {
+      case Some(x) => req.setMarker(x)
+      case None =>  // Do nothing
+    }
+    val result: ListClustersResult = emr.listClusters(req)
+    val builder = Seq.newBuilder[ClusterSummary]
+    for (cs: ClusterSummary <- result.getClusters.asScala if cs.getName.matches(regexp)) builder += cs
+
+    Option(result.getMarker) match {
+      case Some(x) => detectClusters(maker = Some(x)).foreach(cs => builder += cs)
+      case None =>  // Do nothing
+    }
+    builder.result()
+  }
+
+  private def createdAfter: Date = {
+    val zoneId = ZoneId.of(timezone)
+    val zoneOffset = ZoneOffset.of(zoneId.toString)
+    val i = LocalDateTime.now(zoneId)
+      .minusHours(hoursCreatedWithin)
+      .toEpochSecond(zoneOffset)
+    new Date(i * 1000)  // millis
   }
 
 
