844fa1e1bdaf86ddcfcb7fef9f6505cae5bf7eaf
Civitaspo <civitaspo@gmail.com>
2018-07-30 21:58:18 +0900

    Implement emr_fleet.wait_cluster operator

--- src/main/scala/pro/civitaspo/digdag/plugin/emr_fleet/operator/EmrFleetWaitClusterOperator.scala
+++ src/main/scala/pro/civitaspo/digdag/plugin/emr_fleet/operator/EmrFleetWaitClusterOperator.scala
@@ -1,8 +1,8 @@
 package pro.civitaspo.digdag.plugin.emr_fleet.operator
 
-import com.amazonaws.services.elasticmapreduce.model.ClusterState
+import com.amazonaws.services.elasticmapreduce.model.{ClusterState, DescribeClusterRequest, DescribeClusterResult}
 import io.digdag.client.config.Config
-import io.digdag.spi.{OperatorContext, TaskResult, TemplateEngine}
+import io.digdag.spi.{OperatorContext, TaskExecutionException, TaskResult, TemplateEngine}
 import io.digdag.util.DurationParam
 
 import scala.collection.JavaConverters._
@@ -20,6 +20,41 @@ class EmrFleetWaitClusterOperator(
   val timeoutDuration: DurationParam = params.get("timeout_duration", classOf[DurationParam], DurationParam.parse("45m"))
 
   override def runTask(): TaskResult = {
-    null
+    pollingCluster()
+    TaskResult.empty(request)
+  }
+
+  private def pollingCluster(): Unit = {
+    val timeoutSeconds: Int = timeoutDuration.getDuration.getSeconds.toInt
+    val pollingIntervalSeconds: Int = pollingInterval.getDuration.getSeconds.toInt
+    val counter: Iterator[Int] = Stream.from(0).iterator
+    while (!pollCluster) {
+      val timeSpentSeconds: Int = counter.next * pollingIntervalSeconds
+      if (timeSpentSeconds >= timeoutSeconds) {
+        throw new TaskExecutionException(s"timeout => spent: ${timeSpentSeconds}s >= ${timeoutSeconds}s")
+      }
+      Thread.sleep(pollingIntervalSeconds * 1000)  // millis
+    }
+  }
+
+  private def pollCluster: Boolean = {
+    val result: DescribeClusterResult = describeCluster
+    val state: ClusterState = ClusterState.fromValue(result.getCluster.getStatus.getState)
+
+    logger.info(s"current state: ${state.toString}")
+
+    if (errorStates.exists(_.equals(state))) {
+      throw new TaskExecutionException(s"Cluster State is error state: ${state.toString}")
+    }
+    successStates.exists(_.equals(state))
+  }
+
+  private def describeCluster: DescribeClusterResult = {
+     withEmr { emr =>
+       emr.describeCluster(
+         new DescribeClusterRequest()
+           .withClusterId(clusterId)
+       )
+     }
   }
 }
