e92f52b00b5bdc42128b2d76168b9ba07b03528f
Civitaspo <civitaspo@gmail.com>
2018-07-30 14:11:50 +0900

    Setup top level options for emr_fleet.wait_cluster.

--- src/main/scala/pro/civitaspo/digdag/plugin/emr_fleet/operator/EmrFleetWaitClusterOperator.scala
+++ src/main/scala/pro/civitaspo/digdag/plugin/emr_fleet/operator/EmrFleetWaitClusterOperator.scala
@@ -1,7 +1,10 @@
 package pro.civitaspo.digdag.plugin.emr_fleet.operator
 
+import com.amazonaws.services.elasticmapreduce.model.ClusterState
 import io.digdag.client.config.Config
 import io.digdag.spi.{OperatorContext, TaskResult, TemplateEngine}
+
+import scala.collection.JavaConverters._
 
 class EmrFleetWaitClusterOperator(
   context: OperatorContext,
@@ -10,6 +13,10 @@ class EmrFleetWaitClusterOperator(
 ) extends AbstractEmrFleetOperator(context, systemConfig, templateEngine) {
 
   val clusterId: String = params.get("_command", classOf[String])
+  val successStates: Seq[ClusterState] = params.getList("success_states", classOf[ClusterState]).asScala
+  val errorStates: Seq[ClusterState] = params.getListOrEmpty("error_states", classOf[ClusterState]).asScala
+  val pollingIntervalSeconds: Int = params.get("polling_interval_seconds", classOf[Int], 5)
+  val timeoutDurationSeconds: Int = params.get("timeout_duration_seconds", classOf[Int], 45*60)  // 45 min
 
   override def runTask(): TaskResult = {
     null
